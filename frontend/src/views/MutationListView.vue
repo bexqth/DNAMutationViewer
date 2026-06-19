39b651d56a1f385d63005e4cfdb0a1b1484a2b7b
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-06-19 13:28:28 +0200

    fix: table with fixed data

--- frontend/src/views/MutationListView.vue
+++ frontend/src/views/MutationListView.vue
@@ -1,6 +1,96 @@
+<script setup>
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+  import { ref, onMounted } from 'vue';
+  import MutationSideBar from "@/components/mutation-list/MutationSideBar.vue";
+
+  const mutations = ref([
+    { geneName: 'BRCA1', chromosome: '17', position: '43044295', refAllele: 'T', altAllele: 'G', impact: 'high' },
+    { geneName: 'TP53', chromosome: '17', position: '7673767', refAllele: 'C', altAllele: 'A', impact: 'medium' },
+    { geneName: 'EGFR', chromosome: '7', position: '55242465', refAllele: 'A', altAllele: 'T', impact: 'low' }
+  ]);
+
+</script>
+
+
 <template>
-  <div>
-    <h2>Mutation list</h2>
-    <p></p>
+  <div class="mutation-list">
+    <h2 class="title">Mutation list</h2>
+
+    <div class="content-container">
+      <div class="table-container">
+        <DataTable class="data-table" :value="mutations">
+          <Column field="geneName" header="Gene name" sortable style="width: 25%"></Column>
+          <Column field="chromosome" header="Chromosome"></Column>
+          <Column field="position" header="Position"></Column>
+          <Column field="refAllele" header="Ref allele"></Column>
+          <Column field="altAllele" header="Alt allele"></Column>
+          <Column field="impact" header="Impact"></Column>
+        </DataTable>
+      </div>
+
+      <div class="dna-container">
+        <MutationSideBar></MutationSideBar>
+      </div>
+    </div>
+
   </div>
 </template>
+
+<style>
+
+  .title{
+    margin-left: 10px;
+  }
+
+  .mutation-list {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    height: 100%;
+  }
+
+  .mutation-list .content-container {
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    height: 100%;
+    gap: 20px;
+    box-sizing: border-box;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+
+  .table-container {
+    background-color: #fff;
+    border-radius: 20px;
+    flex-grow: 1;
+    min-width: 400px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.03);
+  }
+
+  .data-table {
+    margin: 10px;
+    font-family: "Inter var", "Inter", sans-serif !important;
+  }
+
+  .data-table tr:hover td {
+    background-color: #f8fafc;
+  }
+
+  .dna-container {
+    background-color: #fff;
+    border-radius: 20px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.03);
+    display: flex;
+    min-width: 300px;
+    max-width: 300px;
+  }
+
+  .title{
+    font-family: 'Inter', sans-serif;
+    text-transform: uppercase;
+    color: #0f191f;
+  }
+
+</style>
