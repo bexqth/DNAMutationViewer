d65b4e7514079e4064c67751d16a84f6ee050681
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-06-19 13:28:44 +0200

    feat: mutation side bar

---
+++ frontend/src/components/mutation-list/MutationSideBar.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+import Avatar from "primevue/avatar";
+</script>
+
+<template>
+  <div class="sidebar-container">
+    <div class="dna-avatar-container">
+      <Avatar image="src/assets/gene-mutation_white.png" class="dna-avatar" shape="circle" />
+    </div>
+
+    <div class="dna-nucleotide-container">
+    </div>
+
+    <div class="dna-info-container">
+    </div>
+
+  </div>
+</template>
+
+<style>
+
+  .sidebar-container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    gap: 20px;
+  }
+
+  .dna-avatar-container {
+    flex: 2;
+    flex-shrink: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 0;
+    height: 120px;
+  }
+
+  .dna-avatar {
+    margin-top: 20px;
+    background-color: #83ade2 !important;
+    height: 100% !important;
+    width: auto !important;
+    aspect-ratio: 1 / 1 !important;
+  }
+
+  .dna-avatar img {
+    width: 60% !important;
+    height: 60% !important;
+    object-fit: contain !important;
+    margin: auto;
+  }
+
+  .dna-nucleotide-container {
+    flex: 2;
+  }
+
+  .dna-info-container {
+    flex: 8;
+  }
+</style>
