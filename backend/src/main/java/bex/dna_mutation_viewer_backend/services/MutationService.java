9ceedda59e7edc1f0d2cd2f36e2fe5abc9053943
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-06-23 19:24:57 +0200

    fix: changing service implementation

--- backend/src/main/java/bex/dna_mutation_viewer_backend/services/MutationService.java
+++ backend/src/main/java/bex/dna_mutation_viewer_backend/services/MutationService.java
@@ -1,12 +1,25 @@
 package bex.dna_mutation_viewer_backend.services;
 
+import bex.dna_mutation_viewer_backend.dto.GenomeSettingDto;
 import bex.dna_mutation_viewer_backend.models.Mutation;
+import bex.dna_mutation_viewer_backend.repositories.MutationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-public interface MutationService {
-    Mutation saveMutation(Mutation mutation);
-    List<Mutation> fetchMutationList();
-    //Mutation updateMutation(Mutation mutation, Long mutationId);
-    //void deleteMutationById(Long mutationId);
+@Service
+public class MutationService {
+
+    @Autowired
+    private MutationRepository mutRepository;
+
+    public Mutation saveMutation(Mutation mutation) {
+        return null;
+    }
+
+    public List<Mutation> fetchMutationList() {
+        return (List<Mutation>) mutRepository.findAll();
+    }
 }
