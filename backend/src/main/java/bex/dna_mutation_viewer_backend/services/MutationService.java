b0fbe49f522815a265143b794cf8f9cc1a238da6
Terézia Vrábeľová <vrabelova2@stud.uniza.sk>
2026-06-28 18:48:09 +0200

    fix: loading fasta content as string

--- backend/src/main/java/bex/dna_mutation_viewer_backend/services/MutationService.java
+++ backend/src/main/java/bex/dna_mutation_viewer_backend/services/MutationService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.List;
 
 @Service
@@ -17,9 +18,29 @@ public class MutationService {
 
     public Mutation saveMutation(Mutation mutation) {
         return null;
+
     }
 
     public List<Mutation> fetchMutationList() {
         return (List<Mutation>) mutRepository.findAll();
+    }
+
+    public void analyzeFastaGenomeFile(MultipartFile fastaFile, GenomeSettingDto userSettings) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fastaFile.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null ) {
+                if(line.startsWith(">")) {
+                    continue;
+                }
+                stringBuilder.append(line.trim());
+            }
+            reader.close();
+            String fastaContent = stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
